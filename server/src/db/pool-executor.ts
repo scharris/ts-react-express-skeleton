68d6794adfae83632c5194a47872562fa7ab4189
Steve Harris <stephen.harris@fda.hhs.gov>
2020-10-27 16:42:04 -0500

    Allow env file to be specified via command line arg.

--- server/src/db/pool-executor.ts
+++ server/src/db/pool-executor.ts
@@ -1,29 +1,47 @@
 import Pool from 'pg-pool';
-import {QueryConfig, QueryResult} from 'pg';
+import {Client, QueryConfig, QueryResult} from 'pg';
 
-const env = process.env;
 
-if ( !env.DB_DATABASE || !env.DB_USER || !env.DB_PASSWORD )
-   throw new Error("One or more required database connection values are not defined.");
+let connPool: Pool<Client> | null = null;
 
-const connPool = new Pool({
-   database: env.DB_DATABASE,
-   user: env.DB_USER,
-   password: env.DB_PASSWORD,
-   host: env.DB_HOST || 'localhost',
-   port: parseInt(env.DB_PORT as string, 10) || 5432,
-   ssl:  env.DB_USE_SSL ? env.DB_USE_SSL.toLowerCase() !== 'false' : true,
-   max: parseInt(env.DB_POOL_MAX_SIZE as string, 10) || 2, // set higher for prod
-   connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
-});
+function pool()
+{
+   if ( !connPool )
+      throw new Error('Attempt to use connection pool before it has been initialized.');
+   return connPool;
+}
 
-connPool.on('error', (err, client) => {
-   console.error('pg-pool: Encountered unexpected error on idle client', err)
-});
+export function createPool(): void
+{
+   const env = process.env;
+
+   if ( !env.DB_DATABASE || !env.DB_USER || !env.DB_PASSWORD )
+      throw new Error("One or more required database connection values are not defined.");
+
+   connPool = new Pool({
+      database: env.DB_DATABASE,
+      user: env.DB_USER,
+      password: env.DB_PASSWORD,
+      host: env.DB_HOST || 'localhost',
+      port: parseInt(env.DB_PORT as string, 10) || 5432,
+      ssl:  env.DB_USE_SSL ? env.DB_USE_SSL.toLowerCase() !== 'false' : true,
+      max: parseInt(env.DB_POOL_MAX_SIZE as string, 10) || 2, // set higher for prod
+      connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+   });
+
+   connPool.on('error', (err, client) => {
+      console.error('pg-pool: Encountered unexpected error on idle client', err)
+   });
+}
+
+export async function closePool(): Promise<void>
+{
+   await pool().end();
+}
 
 /** Do a query using the shared connection pool, returning the connection when completed. */
 export async function execSql(sql: string | QueryConfig, params: any[]): Promise<QueryResult>
 {
    console.log(`Executing query`, sql, params);
-   return await connPool.query(sql, params);
+   return await pool().query(sql, params);
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,50 @@
 import express from "express";
 import helmet from "helmet";
+import * as dotenv from 'dotenv';
 import apiRouter from "./api-router";
 import {httpErrorHandler} from "./errors/http-error-handler";
+import {createPool, closePool} from './db/pool-executor';
 
-if ( process.argv.length !== 3 )
+async function init(staticResourcesDir: string, port: number): Promise<void>
 {
-   console.error('Expected application arguments: <public-directory>');
+   createPool();
+   process
+      .once('SIGTERM', closePool)
+      .once('SIGINT',  closePool);
+
+   const app =
+      express()
+         .use(express.static(staticResourcesDir))
+         // CSP is off for now to allow deep links into app content, which require execution of inline scripts.
+         .use(helmet({ contentSecurityPolicy: false }))
+         .use(express.json())
+         .use(express.urlencoded({ extended: true }))
+         .use("/api", apiRouter)
+         // Enable deep links into the app by serving index.html for any routes not recognized above.
+         .get('/*', function (req, res) {
+            res.sendFile('index.html', { root: staticResourcesDir });
+         })
+         .use(httpErrorHandler); // General http errors, other than 404.
+
+   /** Run the server. */
+   app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+   });
+}
+
+////////////////////////////
+//       STARTUP          //
+////////////////////////////
+
+if ( process.argv.length !== 4 )
+{
+   console.error('Expected application arguments: <public-directory> <env-file>');
    process.abort();
 }
 const publicDir = process.argv[2];
+const envFile = process.argv[3];
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT as string, 10): 3000;
+dotenv.config({ path: envFile });
+const port: number = process.env.PORT ? parseInt(process.env.PORT as string, 10): 3000;
 
-const app = express();
-
-app.use(express.static(publicDir));
-// CSP is off for now to allow deep links into app content, which require execution of
-// inline scripts. With more work this could be supported via proper CSP policy like 'nonce'
-// for inline scripts.
-app.use(helmet({ contentSecurityPolicy: false }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", apiRouter);
-
-// Enable deep links into the app by serving index.html for any routes not recognized above.
-app.get('/*', function (req, res) {
-   res.sendFile('index.html', { root: publicDir });
-});
-
-/** error handling */
-app.use(httpErrorHandler); // General http errors, other than 404.
-
-
-/** Run the server. */
-app.listen(PORT, () => {
-   console.log(`Listening on port ${PORT}`);
-});
+init(publicDir, port);
