dbca16c1754e32d781fdb785d9edc805366ddd48
Steve Harris <stephen.harris@fda.hhs.gov>
2020-10-29 11:46:05 -0500

    Change db-init script to use custom postgres imae.

--- db/init-pg.ps1
+++ db/init-pg.ps1
@@ -1,17 +1,16 @@
 $dbInit = "$PSScriptRoot"
 
-docker run -d --name pg --rm `
-  -p 5432:5432 --shm-size=256MB `
-  -v $dbInit/create-schema.sql:/docker-entrypoint-initdb.d/01.sql `
-  -v $dbInit/create-schema-objects.sql:/docker-entrypoint-initdb.d/02.sql `
-  -v $dbInit/create-test-data.sql:/docker-entrypoint-initdb.d/03.sql `
+docker build -t foos-pg $dbInit
+
+docker run -d --name pg --rm -p 5432:5432 --shm-size=256MB `
   -e POSTGRES_USER=foos -e POSTGRES_PASSWORD=foos -e POSTGRES_DB=foos `
-  postgres:13
+  foos-pg
 
 # While container 'pg' started as above is running, you can run a psql client
 # within the container via:
 #    docker exec -it pg psql -U foos
 # This will allow you to enter database queries and commands.
+# Type ctrl-d or "exit" to exit psql and return to your shell.
 #
 # Or connect from the host
 #  - with jdbc via url:
