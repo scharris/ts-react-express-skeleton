26f74deff09051c1adf48146414a6ca4ffaa2410
Steve Harris <stephen.harris@fda.hhs.gov>
2020-12-01 15:00:18 -0600

    Add server shutdown on signals.

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,9 +31,12 @@ async function init(resourcesDir: string, port: number): Promise<void>
          .use(httpErrorHandler); // General http errors, other than 404.
 
    /** Run the server. */
-   app.listen(port, () => {
+   const server = app.listen(port, () => {
       console.log(`Listening on port ${port}`);
    });
+   
+   function exit() { server.close(); closePool(); }
+   process.once('SIGTERM', exit).once('SIGINT',  exit);
 }
 
 ////////////////////////////
